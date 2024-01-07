5e055c44a9d812ad9a6c0e160bb8316dce282246
Andrew Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-01-07 12:34:41 +0200

    loops

--- hello_loops.py
+++ hello_loops.py
@@ -1,7 +1,30 @@
-print("Even or odd?")
-for i in range(5):
-    if i % 2 == 0:
-        print(i, "- even")
-    else:
-        print(i, "- odd")
-print("Done")
+def even_or_odd(n: int = 5):
+    print("Even or odd?")
+    for i in range(n):
+        if i % 2 == 0:
+            print(i, "- even")
+        else:
+            print(i, "- odd")
+    print("Done")
+
+
+def from_n_to_zero(n: int = 5):
+    print('Iterating from N to zero')
+    while n > 0:
+        print('Current number is', n)
+        n = n - 1
+    print('Done')
+
+
+def repeater():
+    while True:
+        line = input('> ')
+        if line.startswith('#'):
+            continue
+        if line == 'exit':
+            break
+        print(line)
+    print('Goodbye!')
+
+
+
