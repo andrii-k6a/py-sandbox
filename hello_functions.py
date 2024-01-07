b66497d48fb4c2253648a4fd901cc3c3c3b564f1
Andrew Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-01-07 12:24:48 +0200

    fibonacci

--- hello_functions.py
+++ hello_functions.py
@@ -7,6 +7,14 @@ def add_magic_number(value: int = 0) -> int:
     return value + 42
 
 
+def fibonacci(n: int):
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
 print('Start')
 print_greetings()
 print("The number is", add_magic_number(7))
