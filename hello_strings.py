e86c5d3528d12fce103e354141a80475626b2f08
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-01-07 23:24:03 +0200

    strings refactoring

--- hello_strings.py
+++ hello_strings.py
@@ -1,7 +1,12 @@
-message = 'the number is '
-number = 42
-print(message + str(number))
+def string_and_number_concatenation():
+    message = 'the number is '
+    number = 42
+    print(message + str(number))
+# string_and_number_concatenation()
 
-a = 7
-b = '123'
-print(a + int(b))
+
+def number_as_string_concatenation():
+    a = 7
+    b = '123'
+    print(a + int(b))
+# number_as_string_concatenation()
