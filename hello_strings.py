b54c2b798fbefe2fffa5b9fa2c4c9b599d2667cf
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-01-07 23:59:50 +0200

    more strings examples

--- hello_strings.py
+++ hello_strings.py
@@ -2,11 +2,22 @@ def string_and_number_concatenation():
     message = 'the number is '
     number = 42
     print(message + str(number))
-# string_and_number_concatenation()
 
 
 def number_as_string_concatenation():
     a = 7
     b = '123'
     print(a + int(b))
-# number_as_string_concatenation()
+
+
+def print_letter():
+    text = 'Hello There!'
+    letter = text[6]
+    print(letter)
+
+
+def print_text_len():
+    text = "This is text"
+    print(text)
+    print('The length of the text is', len(text))
+
