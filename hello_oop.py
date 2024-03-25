33f00fa03103b913eb87a3eca817d451c6f07847
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-03-25 21:50:08 +0200

    construction/destruction

--- hello_oop.py
+++ hello_oop.py
@@ -1,12 +1,20 @@
 class Greetings:
+    greeting = ''
     greetings_counter = 0
+
+    def __init__(self, greeting):
+        self.greeting = greeting
+        print('Greetings constructor', self.greeting, self.greetings_counter)
 
     def hello(self):
         self.greetings_counter += 1
-        print(self.greetings_counter, 'Hello')
+        print(self.greetings_counter, self.greeting)
+
+    def __del__(self):
+        print('Greetings destructor', self.greeting, self.greetings_counter)
 
 
-g1 = Greetings()  # Constructing an instance
+g1 = Greetings('hello')  # Constructing an instance
 g1.hello()  # Equivalent: Greetings.hello(g1)
 g1.hello()
 g1.hello()
@@ -14,7 +22,9 @@ Greetings.hello(g1)
 print(type(g1))
 print(dir(g1))
 
-g2 = Greetings()
+g1 = 'Destructor supposed to be called at this point'
+
+g2 = Greetings('hola')
 g2.hello()
 g2.hello()
 
