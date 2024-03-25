93305e8f158f48159a7bae81adde624fb5592650
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-03-25 14:31:40 +0200

    execution time decorator

--- hello_decorators.py
+++ hello_decorators.py
@@ -1,3 +1,7 @@
+import time
+import math
+
+
 def greetings_decorator(greetings_func):
     print('Before greetings')
     greetings_func()
@@ -9,3 +13,25 @@ def greetings_decorator(greetings_func):
 def greetings():
     print('Hello')
 
+
+def execution_time_decorator(func):
+    print('Adding execution time decorator...')
+
+    def wrapper_func(*args, **kwargs):
+        begin = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print("Total time taken in : ", func.__name__, end - begin)
+
+    return wrapper_func
+
+
+# @execution_time_decorator actually calls the execution_time_decorator function
+# Equivalent would look like adding 'factorial = execution_time_decorator(factorial)' after factorial func declaration
+@execution_time_decorator
+def factorial(num):
+    time.sleep(1)
+    print('Result:', math.factorial(num))
+
+
+factorial(10)
