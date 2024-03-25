b61b26d35488701693d7fcc039ff05313fe14dd7
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-03-25 15:43:02 +0200

    hello oop

---
+++ hello_oop.py
@@ -0,0 +1,19 @@
+class Greetings:
+    greetings_counter = 0
+
+    def hello(self):
+        self.greetings_counter += 1
+        print(self.greetings_counter, 'Hello')
+
+
+g1 = Greetings()  # Constructing an instance
+g1.hello()  # Equivalent: Greetings.hello(g1)
+g1.hello()
+g1.hello()
+Greetings.hello(g1)
+
+print('***')
+g2 = Greetings()
+g2.hello()
+g2.hello()
+
