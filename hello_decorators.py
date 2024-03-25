c361f400156563524c5b4829c9041fcbc89a5ee2
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-03-25 14:06:17 +0200

    hello decorators

---
+++ hello_decorators.py
@@ -0,0 +1,11 @@
+def greetings_decorator(greetings_func):
+    print('Before greetings')
+    greetings_func()
+    print('After greetings')
+
+
+# @greetings_decorator calls a function, not just declares
+@greetings_decorator
+def greetings():
+    print('Hello')
+
