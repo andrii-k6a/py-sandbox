c951ce87b795e2dbeace06198e585bdc4f2957e2
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-04-01 13:01:46 +0300

    decorators with args

--- hello_decorators.py
+++ hello_decorators.py
@@ -91,3 +91,36 @@ def num2():
 
 print('About to call num2()')
 print('Num2 result', num2())  # 142
+
+
+print('\n*** Decorators with args ***')
+
+
+def foo(number, description):
+    print('hello from foo -', number, description)
+
+    def inner_decorator(f):
+        print('hello from foo.inner_decorator')
+
+        def wrapped(*args, **kwargs):
+            print('hello from foo.inner_decorator.wrapped')
+            result = f(*args, **kwargs)
+            print('bye from foo.inner_decorator.wrapped')
+            return result
+
+        print('bye from foo.inner_decorator')
+        return wrapped
+
+    print('bye from foo')
+    return inner_decorator
+
+
+@foo(42, 'example')
+def bar():
+    print('hello from bar')
+    return 'I am bar func'
+# Equivalent: 'bar = foo(42, 'example')(bar)'
+
+
+print('Calling bar!..')
+print(bar())
