be72eda4abfb6e612b4fdbdcb446e95ff30f4a6a
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-05-02 15:15:40 +0300

    hello sqlite3

---
+++ db/hello_sqlite3.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+conn = sqlite3.connect('hello_sqlite.db')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS People')
+
+cur.execute('CREATE TABLE People (email TEXT, age INTEGER)')
+
+while input('Do you want to add a user? (yes/no) ') == 'yes':
+    email = input('Enter email: ')
+    age = input('Enter age: ')
+
+    cur.execute('SELECT email FROM People WHERE email = ? ', (email,))
+    row = cur.fetchone()
+
+    if row is not None:
+        print('There is an existing user with email', email, '- updating the data')
+        cur.execute('UPDATE People SET age = ? WHERE email = ?', (age, email))
+    else:
+        print('Inserting a new user')
+        cur.execute('INSERT INTO People (email, age) VALUES (?, ?)', (email, age))
+
+    conn.commit()
+
+print('Goodbye!')
