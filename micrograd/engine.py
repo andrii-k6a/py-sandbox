e9d1ac56803a363f3ce8b9ff9b3258080a5ce302
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-09-08 13:07:21 +0300

    start building micrograd from scratch

---
+++ micrograd/engine.py
@@ -0,0 +1,19 @@
+class Value:
+
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Value={self.data}"
+
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
---
+++ micrograd/runner.py
@@ -0,0 +1,24 @@
+from micrograd.engine import Value
+from micrograd.viewer import draw_dot
+
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+
+d = a * b  # -6.0
+d.label = 'd'
+
+e = d + c  # 4.0
+e.label = 'e'
+
+f = Value(-2.0, label='f')
+
+G = e * f  # -8.0; G = (a*b + c) * f = a*b*f + c*f
+G.label = 'G'
+
+print(G)
+print(G._prev)
+print(G._op)
+
+dot = draw_dot(G)
+dot.render('graph', format='png')  # Save the graph as a PNG file
