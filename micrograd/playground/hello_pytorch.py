614d933c3cc1f6c3c6af24161e2b0d3e9c0755ac
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-09-12 23:02:13 +0300

    numpy and torch examples

---
+++ micrograd/playground/hello_pytorch.py
@@ -0,0 +1,19 @@
+import torch
+
+x1 = torch.Tensor([2.0]).double()                ; x1.requires_grad = True
+x2 = torch.Tensor([0.0]).double()                ; x2.requires_grad = True
+w1 = torch.Tensor([-3.0]).double()               ; w1.requires_grad = True
+w2 = torch.Tensor([1.0]).double()                ; w2.requires_grad = True
+b = torch.Tensor([6.8813735870195432]).double()  ; b.requires_grad = True
+n = x1*w1 + x2*w2 + b
+o = torch.tanh(n)
+
+print(o.data.item())
+
+o.backward()
+
+print('---')
+print('x2', x2.grad.item())
+print('w2', w2.grad.item())
+print('x1', x1.grad.item())
+print('w1', w1.grad.item())
