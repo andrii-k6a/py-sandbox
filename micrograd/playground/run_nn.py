7615be3aee9241f10f4d47180d5d9ccf53c01bd4
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-09-13 19:36:14 +0300

    neural net impl

---
+++ micrograd/playground/run_nn.py
@@ -0,0 +1,13 @@
+from micrograd.nn import MLP
+from micrograd.viewer import draw_dot
+
+inp = [2.0, 3.0, -1.0]
+mlp = MLP(3, [4, 4, 1])
+print(mlp)
+
+res = mlp(inp)
+
+print(res)
+
+dot = draw_dot(res)
+dot.render('mlp', format='png')
