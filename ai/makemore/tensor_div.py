d800337b8275f0ded637f2f957cfa7aeb1b65f50
Andrii Kook <102363876+andrii-k6a@users.noreply.github.com>
2024-10-08 13:16:21 +0300

    broadcasting semantics

--- ai/makemore/tensor_div.py
+++ ai/makemore/tensor_div.py
@@ -11,7 +11,11 @@ print(tensor_sum)
 # tensor([[ 6],
 #         [24]])
 
+# broadcasting semantics:
+# https://pytorch.org/docs/stable/notes/broadcasting.html
+# https://numpy.org/doc/stable/user/basics.broadcasting.html
 res = tensor / tensor_sum
 print(res)
+# the smaller array is “broadcast” across the larger array so that they have compatible shapes
 # tensor([[2/6=0.3333, 4/6=0.6667],
 #         [8/24=0.3333, 16/24=0.6667]])
